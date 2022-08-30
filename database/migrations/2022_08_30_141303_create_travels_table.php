4d09c6a6abdbc9c1395438684a54badfa8af883f
franciowel <spina_francesco@blu.it>
2022-08-30 16:27:15 +0200

    created seed

--- database/migrations/2022_08_30_141303_create_travels_table.php
+++ database/migrations/2022_08_30_141303_create_travels_table.php
@@ -15,6 +15,13 @@ class CreateTravelsTable extends Migration
     {
         Schema::create('travels', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 70);
+            $table->string('destination', 150);
+            $table->date('start_date');
+            $table->tinyInteger('days')->unsigned();
+            $table->decimal('price', 7, 2);
+            $table->string('vehicle', 20);
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
